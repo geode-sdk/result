d1251ac30f564877303e5b22c570b2178d68d30e
alk <45172705+altalk23@users.noreply.github.com>
2024-11-21 09:48:31 +0300

    move isresult out

--- include/Geode/Result.hpp
+++ include/Geode/Result.hpp
@@ -234,9 +234,6 @@ namespace geode {
         };
 
         template <class Type>
-        concept IsResult = IsResultImpl<Type>::value;
-
-        template <class Type>
         using ResultOkType = typename IsResultImpl<Type>::OkType;
 
         template <class Type>
@@ -249,6 +246,9 @@ namespace geode {
         template <class Type>
         concept IsStringStreamable = requires(std::stringstream ss, Type t) { ss << t; };
     }
+
+    template <class Type>
+    concept IsResult = impl::IsResultImpl<Type>::value;
 
     /// @brief Constructs a new Ok value
     /// @param ok the value to wrap in an Ok
@@ -1591,7 +1591,7 @@ namespace geode {
         /// @param operation the operation to perform if this Result is Ok
         /// @return the result of the operation if this Result is Ok, otherwise this Result
         template <class Operation>
-            requires(std::invocable<Operation, OkType> && impl::IsResult<std::invoke_result_t<Operation, OkType>> && std::convertible_to<ErrType, impl::ResultErrType<std::invoke_result_t<Operation, OkType>>>)
+            requires(std::invocable<Operation, OkType> && IsResult<std::invoke_result_t<Operation, OkType>> && std::convertible_to<ErrType, impl::ResultErrType<std::invoke_result_t<Operation, OkType>>>)
         constexpr std::invoke_result_t<Operation, OkType> andThen(Operation&& operation
         ) && noexcept(noexcept(operation(std::declval<OkType>()))) {
             if (this->isOk()) {
@@ -1606,7 +1606,7 @@ namespace geode {
         /// @param operation the operation to perform if this Result is Ok
         /// @return the result of the operation if this Result is Ok, otherwise this Result
         template <class Operation>
-            requires(std::invocable<Operation, OkType> && impl::IsResult<std::invoke_result_t<Operation, OkType>> && std::convertible_to<ErrType, impl::ResultErrType<std::invoke_result_t<Operation, OkType>>>)
+            requires(std::invocable<Operation, OkType> && IsResult<std::invoke_result_t<Operation, OkType>> && std::convertible_to<ErrType, impl::ResultErrType<std::invoke_result_t<Operation, OkType>>>)
         constexpr std::invoke_result_t<Operation, OkType> andThen(Operation&& operation
         ) const& noexcept(noexcept(operation(std::declval<OkType>()))) {
             if (this->isOk()) {
@@ -1621,7 +1621,7 @@ namespace geode {
         /// @param operation the operation to perform if this Result is Ok
         /// @return the result of the operation if this Result is Ok, otherwise this Result
         template <class Operation>
-            requires(std::invocable<Operation> && impl::IsResult<std::invoke_result_t<Operation>> && std::convertible_to<ErrType, impl::ResultErrType<std::invoke_result_t<Operation>>>)
+            requires(std::invocable<Operation> && IsResult<std::invoke_result_t<Operation>> && std::convertible_to<ErrType, impl::ResultErrType<std::invoke_result_t<Operation>>>)
         constexpr std::invoke_result_t<Operation> andThen(Operation&& operation
         ) && noexcept(noexcept(operation())) {
             if (this->isOk()) {
@@ -1636,7 +1636,7 @@ namespace geode {
         /// @param operation the operation to perform if this Result is Ok
         /// @return the result of the operation if this Result is Ok, otherwise this Result
         template <class Operation>
-            requires(std::invocable<Operation> && impl::IsResult<std::invoke_result_t<Operation>> && std::convertible_to<ErrType, impl::ResultErrType<std::invoke_result_t<Operation>>>)
+            requires(std::invocable<Operation> && IsResult<std::invoke_result_t<Operation>> && std::convertible_to<ErrType, impl::ResultErrType<std::invoke_result_t<Operation>>>)
         constexpr std::invoke_result_t<Operation> andThen(Operation&& operation
         ) const& noexcept(noexcept(operation())) {
             if (this->isOk()) {
@@ -1677,7 +1677,7 @@ namespace geode {
         /// @param operation the operation to perform if this Result is Err
         /// @return the result of the operation if this Result is Err, otherwise this Result
         template <class Operation>
-            requires(std::invocable<Operation, ErrType> && impl::IsResult<std::invoke_result_t<Operation, ErrType>> && std::convertible_to<OkType, impl::ResultOkType<std::invoke_result_t<Operation, ErrType>>>)
+            requires(std::invocable<Operation, ErrType> && IsResult<std::invoke_result_t<Operation, ErrType>> && std::convertible_to<OkType, impl::ResultOkType<std::invoke_result_t<Operation, ErrType>>>)
         constexpr std::invoke_result_t<Operation, ErrType> orElse(Operation&& operation
         ) && noexcept(noexcept(operation(std::declval<ErrType>()))) {
             if (this->isOk()) {
@@ -1692,7 +1692,7 @@ namespace geode {
         /// @param operation the operation to perform if this Result is Err
         /// @return the result of the operation if this Result is Err, otherwise this Result
         template <class Operation>
-            requires(std::invocable<Operation, ErrType> && impl::IsResult<std::invoke_result_t<Operation, ErrType>> && std::convertible_to<OkType, impl::ResultOkType<std::invoke_result_t<Operation, ErrType>>>)
+            requires(std::invocable<Operation, ErrType> && IsResult<std::invoke_result_t<Operation, ErrType>> && std::convertible_to<OkType, impl::ResultOkType<std::invoke_result_t<Operation, ErrType>>>)
         constexpr std::invoke_result_t<Operation, ErrType> orElse(Operation&& operation
         ) const& noexcept(noexcept(operation(std::declval<ErrType>()))) {
             if (this->isOk()) {
@@ -1707,7 +1707,7 @@ namespace geode {
         /// @param operation the operation to perform if this Result is Err
         /// @return the result of the operation if this Result is Err, otherwise this Result
         template <class Operation>
-            requires(std::invocable<Operation> && impl::IsResult<std::invoke_result_t<Operation>> && std::convertible_to<OkType, impl::ResultOkType<std::invoke_result_t<Operation>>>)
+            requires(std::invocable<Operation> && IsResult<std::invoke_result_t<Operation>> && std::convertible_to<OkType, impl::ResultOkType<std::invoke_result_t<Operation>>>)
         constexpr std::invoke_result_t<Operation> orElse(Operation&& operation
         ) && noexcept(noexcept(operation())) {
             if (this->isOk()) {
@@ -1722,7 +1722,7 @@ namespace geode {
         /// @param operation the operation to perform if this Result is Err
         /// @return the result of the operation if this Result is Err, otherwise this Result
         template <class Operation>
-            requires(std::invocable<Operation> && impl::IsResult<std::invoke_result_t<Operation>> && std::convertible_to<OkType, impl::ResultOkType<std::invoke_result_t<Operation>>>)
+            requires(std::invocable<Operation> && IsResult<std::invoke_result_t<Operation>> && std::convertible_to<OkType, impl::ResultOkType<std::invoke_result_t<Operation>>>)
         constexpr std::invoke_result_t<Operation> orElse(Operation&& operation
         ) const& noexcept(noexcept(operation())) {
             if (this->isOk()) {
@@ -1779,7 +1779,7 @@ namespace geode {
         /// @return the inner Result if the Result is Ok, otherwise the outer Result
         constexpr Result<impl::ResultOkType<OkType>, ErrType> flatten(
         ) && noexcept(std::is_nothrow_move_constructible_v<OkType> && std::is_nothrow_move_constructible_v<ErrType>)
-            requires(impl::IsResult<OkType> && std::same_as<ErrType, impl::ResultErrType<OkType>>)
+            requires(IsResult<OkType> && std::same_as<ErrType, impl::ResultErrType<OkType>>)
         {
             if (this->isOk()) {
                 return std::move(*this).unwrap();
@@ -1793,7 +1793,7 @@ namespace geode {
         /// @return the inner Result if the Result is Ok, otherwise the outer Result
         constexpr Result<impl::ResultOkType<OkType>, ErrType> flatten(
         ) const& noexcept(std::is_nothrow_move_constructible_v<OkType> && std::is_nothrow_move_constructible_v<ErrType>)
-            requires(impl::IsResult<OkType> && std::same_as<ErrType, impl::ResultErrType<OkType>>)
+            requires(IsResult<OkType> && std::same_as<ErrType, impl::ResultErrType<OkType>>)
         {
             if (this->isOk()) {
                 return this->unwrap();
